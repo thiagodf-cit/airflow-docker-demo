23923d1f2eb16fec17f0825e1f4a93ddbff48a53
thiagodf <thiagodf@ciandt.com>
2020-04-01 18:12:06 -0300

    edit fix

--- dags/trade_etanol_anidro.py
+++ dags/trade_etanol_anidro.py
@@ -3,7 +3,7 @@
 # --------------------------------------------------------------------------------
 import airflow
 from airflow import DAG
-# from airflow.operators.bash_operator import BashOperator
-# from airflow.operators.python_operator import PythonOperator
-# from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.hive_operator import HiveOperator
 from datetime import date, timedelta

e2986dbba21b9db6d2bfc85d8509174128df965e
thiagodf <thiagodf@ciandt.com>
2020-04-05 12:30:46 -0300

    fix ok in db

--- dags/trade_etanol_anidro.py
+++ dags/trade_etanol_anidro.py
@@ -41,29 +41,36 @@ def formating_file(**kwargs):
         sep=';',
         decimal='.',
         encoding=encoding,
-        parse_dates=['date_trade'],
+        parse_dates=['ref_date'],
         header=None,
-        names=['date_trade',
+        names=['ref_date',
                'value_per_liter_brl',
                'value_per_liter_usd',
                'weekly_variation'])
 
-    file_csv.to_csv( local_path_formated + file_formated, index=False)
+    file_csv.to_csv( local_path_formated + file_formated, sep=',', index=False)
     file_formated_path = local_path_formated + file_formated
     file_formated = pd.read_csv(file_formated_path)
     print(file_formated)
     return file_formated_path
     
-def insert_in_db(**kwargs):
-    print("Insert data in db etanol: ")
+def insert_in_db(db_table_name, **kwargs):
+    print("Insert data in db etanol....\n")
+
+    from sqlalchemy import create_engine
+    mysql_engine = create_engine('mysql://{0}:{1}@{2}:{3}/{4}'.format('root', 'root', 'trade-mysql', '3306', 'trade'))
+    # existing_databases = mysql_engine.execute("SHOW DATABASES;")
+    # print(existing_databases)
+    
     file_formated_path = kwargs['ti'].xcom_pull(task_ids='formating_file')
-    file_formated = pd.read_csv(file_formated_path, header=None)
+    file_formated = pd.read_csv(file_formated_path)
+    file_formated.to_sql(db_table_name, mysql_engine, if_exists='append', index=False)
     print(file_formated)
     
-    tb_db = 'etanol_anidro'
-    conn = MySqlHook(conn_name_attr = 'mysql_conn_id', mysql_conn_id='trade-mysql')
-    conn.bulk_load(tb_db, file_formated_path)
-    return tb_db
+    # tb_db = 'etanol_anidro'
+    # conn = MySqlHook(conn_name_attr = 'mysql_conn_id', mysql_conn_id='trade-mysql')
+    # conn.bulk_load(tb_db, file_formated_path)
+    # return tb_db
     
 # --------------------------------------------------------------------------------
 # Init the DAG
@@ -100,15 +107,16 @@ with DAG(
     # pega o arquivo original e formata ele
     formating_file = PythonOperator(
         task_id="formating_file",
-        python_callable=formating_file,
-        provide_context=True)
+        provide_context=True,
+        python_callable=formating_file)
     
     # Test => docker-compose -f docker-compose.yml run --rm webserver airflow test trade_etanol_anidro insert_in_db 2020-03-29
     # pega o arquivo formatado e salva os dados no db
     insert_in_db = PythonOperator(
         task_id="insert_in_db",
+        provide_context=True,
         python_callable=insert_in_db,
-        provide_context=True)
+        op_kwargs={ 'db_table_name': 'etanol_anidro' })
     
     # Test => docker-compose -f docker-compose.yml run --rm webserver airflow test trade_etanol_anidro insert_in_db 2020-03-29
     # apaga o arquivo formatado
